0e4e813f2d06bd654451605374869ed052a57655
Neo <hu.yu.huei@gmail.com>
2017-08-19 22:56:26 +0800

    語言設定

--- app/src/main/java/com/whatmedia/ttia/page/main/language/LanguageSettingFragment.java
+++ app/src/main/java/com/whatmedia/ttia/page/main/language/LanguageSettingFragment.java
@@ -135,13 +135,16 @@ public class LanguageSettingFragment extends BaseFragment implements LanguageSet
                             new AlertDialog.Builder(getContext())
                                     .setTitle(R.string.note)
                                     .setMessage(getString(R.string.language_setting_select_confirm_dialog, finalString))
-                                    .setPositiveButton(R.string.alert_btn_ok, null)
+                                    .setPositiveButton(R.string.alert_btn_ok, new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            Intent i = getActivity().getIntent();
+                                            getActivity().finish();
+                                            getActivity().startActivity(i);
+                                        }
+                                    })
                                     .show();
 //                            mAdapter.setData();
-
-                            Intent i = getActivity().getIntent();
-                            getActivity().finish();
-                            getActivity().startActivity(i);
                         }
                     })
                     .setNegativeButton(R.string.alert_btn_cancel, null)
