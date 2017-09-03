a26d78ad659de3ab87c6b53225f5d47334a90e61
Neo <hu.yu.huei@gmail.com>
2017-09-03 17:38:18 +0800

    FCM如果APP在前台自己推

--- app/src/main/java/com/whatmedia/ttia/services/FCMMessageService.java
+++ app/src/main/java/com/whatmedia/ttia/services/FCMMessageService.java
@@ -1,9 +1,17 @@
 package com.whatmedia.ttia.services;
 
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+import com.whatmedia.ttia.R;
+import com.whatmedia.ttia.page.main.MainActivity;
+
+import java.util.Random;
 
 /**
  * Created by neo_mac on 2017/8/21.
@@ -13,5 +21,21 @@ public class FCMMessageService extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         Log.d("FCM", "onMessageReceived:" + remoteMessage.getFrom());
+
+        Notification.Builder builder = new Notification.Builder(this);
+        builder.setContentTitle(remoteMessage.getNotification().getTitle())
+                .setContentText(remoteMessage.getNotification().getBody())
+                .setDefaults(Notification.DEFAULT_VIBRATE)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setAutoCancel(true);
+
+        //Click intent
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
+                new Intent(this, MainActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP), PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(contentIntent);
+
+        Notification notificationCompat = builder.build();
+        NotificationManagerCompat managerCompat = NotificationManagerCompat.from(this);
+        managerCompat.notify(new Random().nextInt(900) + 65, notificationCompat);
     }
 }
