7c5e80708bbce1c0e2b219027c1c031ad589ed0c
Neo <hu.yu.huei@gmail.com>
2017-09-13 01:41:45 +0800

    首頁停車場UI調整

--- app/src/main/java/com/whatmedia/ttia/page/main/home/parking/HomeParkingInfoFragment.java
+++ app/src/main/java/com/whatmedia/ttia/page/main/home/parking/HomeParkingInfoFragment.java
@@ -3,19 +3,18 @@ package com.whatmedia.ttia.page.main.home.parking;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import android.support.v4.content.ContextCompat;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.whatmedia.ttia.R;
 import com.whatmedia.ttia.page.BaseFragment;
 import com.whatmedia.ttia.page.IActivityTools;
 import com.whatmedia.ttia.response.data.HomeParkingInfoData;
-import com.whatmedia.ttia.utility.Util;
 
 import java.util.List;
 
@@ -24,8 +23,26 @@ import butterknife.ButterKnife;
 
 public class HomeParkingInfoFragment extends BaseFragment implements HomeParkingInfoContract.View {
     private static final String TAG = HomeParkingInfoFragment.class.getSimpleName();
-    @BindView(R.id.recyclerView)
-    RecyclerView mRecyclerView;
+    @BindView(R.id.textView_name_P1)
+    TextView mTextViewNameP1;
+    @BindView(R.id.textView_count_P1)
+    TextView mTextViewCountP1;
+    @BindView(R.id.textView_name_P2)
+    TextView mTextViewNameP2;
+    @BindView(R.id.textView_count_P2)
+    TextView mTextViewCountP2;
+    @BindView(R.id.textView_name_P3)
+    TextView mTextViewNameP3;
+    @BindView(R.id.textView_sub_name_P3)
+    TextView mTextViewSubNameP3;
+    @BindView(R.id.textView_count_P3)
+    TextView mTextViewCountP3;
+    @BindView(R.id.textView_name_P4)
+    TextView mTextViewNameP4;
+    @BindView(R.id.textView_sub_name_P4)
+    TextView mTextViewSubNameP4;
+    @BindView(R.id.textView_count_P4)
+    TextView mTextViewCountP4;
 
 
     private IActivityTools.ILoadingView mLoadingView;
@@ -64,9 +81,6 @@ public class HomeParkingInfoFragment extends BaseFragment implements HomeParking
 
         mPresenter = HomeParkingInfoPresenter.getInstance(getContext(), this);
         mPresenter.getParkingInfoAPI();
-        mAdapter = new HomeParkingInfoRecyclerViewAdapter(getContext());
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
-        mRecyclerView.setAdapter(mAdapter);
         return view;
     }
 
@@ -106,12 +120,55 @@ public class HomeParkingInfoFragment extends BaseFragment implements HomeParking
     @Override
     public void getParkingInfoSucceed(final List<HomeParkingInfoData> result) {
         if (isAdded() && !isDetached()) {
-            mMainActivity.runOnUI(new Runnable() {
-                @Override
-                public void run() {
-                    mAdapter.setData(result);
-                }
-            });
+            if (result!=null) {
+                mMainActivity.runOnUI(new Runnable() {
+                    @Override
+                    public void run() {
+                        mTextViewNameP1.setText(getString(R.string.parking_info_parking_space_P1));
+                        mTextViewNameP2.setText(getString(R.string.parking_info_parking_space_P2));
+                        mTextViewNameP4.setText(getString(R.string.parking_info_parking_space_P4));
+                        mTextViewSubNameP4.setText(getString(R.string.parking_info_parking_space_B1_B2));
+                        mTextViewNameP3.setText(getString(R.string.parking_info_parking_space_P4));
+                        mTextViewSubNameP3.setText(getString(R.string.parking_info_parking_space_1F));
+
+                        String count1 = !TextUtils.isEmpty(result.get(0).getAvailableCar()) ? result.get(0).getAvailableCar() : "0";
+                        String count2 = !TextUtils.isEmpty(result.get(1).getAvailableCar()) ? result.get(1).getAvailableCar() : "0";
+                        String count3 = !TextUtils.isEmpty(result.get(2).getAvailableCar()) ? result.get(2).getAvailableCar() : "0";
+                        String count4 = !TextUtils.isEmpty(result.get(3).getAvailableCar()) ? result.get(3).getAvailableCar() : "0";
+                        if (!TextUtils.equals(count1, "0")) {
+                            mTextViewCountP1.setText(getString(R.string.parking_info_parking_space_number_of, count1));
+                            mTextViewCountP1.setTextColor(ContextCompat.getColor(getContext(), android.R.color.white));
+                        } else {
+                            mTextViewCountP1.setText(getString(R.string.parking_info_parking_space_full_state));
+                            mTextViewCountP1.setTextColor(ContextCompat.getColor(getContext(), R.color.colorTextHomeParkingInfoFull));
+                        }
+                        if (!TextUtils.equals(count2, "0")) {
+                            mTextViewCountP2.setText(getString(R.string.parking_info_parking_space_number_of, count2));
+                            mTextViewCountP2.setTextColor(ContextCompat.getColor(getContext(), android.R.color.white));
+                        } else {
+                            mTextViewCountP2.setText(getString(R.string.parking_info_parking_space_full_state));
+                            mTextViewCountP2.setTextColor(ContextCompat.getColor(getContext(), R.color.colorTextHomeParkingInfoFull));
+                        }
+                        if (!TextUtils.equals(count3, "0")) {
+                            mTextViewCountP4.setText(getString(R.string.parking_info_parking_space_number_of, count3));
+                            mTextViewCountP4.setTextColor(ContextCompat.getColor(getContext(), android.R.color.white));
+                        } else {
+                            mTextViewCountP4.setText(getString(R.string.parking_info_parking_space_full_state));
+                            mTextViewCountP4.setTextColor(ContextCompat.getColor(getContext(), R.color.colorTextHomeParkingInfoFull));
+                        }
+                        if (!TextUtils.equals(count4, "0")) {
+                            mTextViewCountP3.setText(getString(R.string.parking_info_parking_space_number_of, count4));
+                            mTextViewCountP3.setTextColor(ContextCompat.getColor(getContext(), android.R.color.white));
+                        } else {
+                            mTextViewCountP3.setText(getString(R.string.parking_info_parking_space_full_state));
+                            mTextViewCountP3.setTextColor(ContextCompat.getColor(getContext(), R.color.colorTextHomeParkingInfoFull));
+                        }
+                    }
+//                    mAdapter.setData(result);
+                });
+            }else {
+                Log.e(TAG, "getParkingInfoSucceed Response is null");
+            }
         } else {
             Log.d(TAG, "Fragment is not add");
         }
