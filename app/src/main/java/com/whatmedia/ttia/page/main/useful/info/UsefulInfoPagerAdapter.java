269fd7d0fb9a9ef306563b55ae8d010737800c80
ian Lin <ian.lin@ufispace.com>
2017-12-06 16:55:35 +0800

    [feature] 版面更新(大區塊的)

---
+++ app/src/main/java/com/whatmedia/ttia/page/main/useful/info/UsefulInfoPagerAdapter.java
@@ -0,0 +1,75 @@
+package com.whatmedia.ttia.page.main.useful.info;
+
+
+import android.support.v4.view.PagerAdapter;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.whatmedia.ttia.R;
+import com.whatmedia.ttia.enums.UsefulInfo;
+import com.whatmedia.ttia.interfaces.IOnItemClickListener;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class UsefulInfoPagerAdapter extends PagerAdapter implements IOnItemClickListener {
+    private final static String TAG = UsefulInfoPagerAdapter.class.getSimpleName();
+
+    private UsefulInfoRecyclerViewAdapter mAdapter;
+    private List<List<UsefulInfo>> mItems = UsefulInfo.getPageList();
+    private IOnItemClickListener mListener;
+
+    @Override
+    public int getCount() {
+        return mItems.size() % 8;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        View view = LayoutInflater.from(container.getContext()).inflate(R.layout.item_home_feature, container, false);
+
+        final ViewHolder holder = new ViewHolder(view);
+        mAdapter = new UsefulInfoRecyclerViewAdapter(container.getContext(), mItems.get(position));
+        holder.mRecyclerView.setLayoutManager(new GridLayoutManager(container.getContext(), 2));
+        holder.mRecyclerView.setAdapter(mAdapter);
+        mAdapter.setClickListener(this);
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (mListener != null)
+            mListener.onClick(view);
+    }
+
+    public void setClickListener(IOnItemClickListener listener) {
+        mListener = listener;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+//        super.destroyItem(container, position, object);
+        container.removeView((View) object);
+        object = null;
+    }
+
+    class ViewHolder {
+        @BindView(R.id.recyclerView)
+        RecyclerView mRecyclerView;
+
+        ViewHolder(View view) {
+            ButterKnife.bind(this, view);
+        }
+    }
+}
