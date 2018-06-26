599dc1918b15c48308959a4c63f8fb7af9ec9dec
Neo <hu.yu.huei@gmail.com>
2018-06-26 22:43:02 +0800

    預防crash

--- app/src/main/java/com/whatsmedia/ttia/newresponse/GetLanguageListResponse.java
+++ app/src/main/java/com/whatsmedia/ttia/newresponse/GetLanguageListResponse.java
@@ -2,8 +2,10 @@ package com.whatsmedia.ttia.newresponse;
 
 import android.content.Context;
 import android.text.TextUtils;
+import android.util.Log;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
 import com.google.gson.annotations.SerializedName;
 import com.whatsmedia.ttia.newresponse.data.LanguageListData;
 import com.whatsmedia.ttia.utility.Preferences;
@@ -15,6 +17,8 @@ import java.util.List;
  */
 
 public class GetLanguageListResponse extends GetBaseResponse {
+    private final static String TAG = GetLanguageListResponse.class.getSimpleName();
+
     @SerializedName("langList")
     private List<LanguageListData> data;
 
@@ -22,8 +26,15 @@ public class GetLanguageListResponse extends GetBaseResponse {
         Gson gson = new Gson();
 
         if (!TextUtils.isEmpty(response)) {
-            Preferences.saveLanguageList(context, response);
-            return gson.fromJson(response, GetLanguageListResponse.class);
+            GetLanguageListResponse getLanguageListResponse;
+            try {
+                Preferences.saveLanguageList(context, response);
+                getLanguageListResponse = gson.fromJson(response, GetLanguageListResponse.class);
+            } catch (JsonSyntaxException e) {
+                Log.e(TAG, "[GetLanguageListResponse getGson is not Json]", e);
+                getLanguageListResponse = new GetLanguageListResponse();
+            }
+            return getLanguageListResponse;
         } else {
             if (!TextUtils.isEmpty(Preferences.getLanguageList(context))) {
                 return gson.fromJson((Preferences.getLanguageList(context)), GetLanguageListResponse.class);
